7d04a593c0e57ed6980cdc02d3b4eee5a66fc63a
tasneemzahran1 <tasneemzahran702@gmail.com>
2025-02-15 19:37:56 +0200

    error-page

--- src/components/Notfound/Notfound.jsx
+++ src/components/Notfound/Notfound.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import style from "./Notfound.module.css"
 export default function Notfound() {
-  return (
-    <>
-      <h1>Notfound</h1>
-    </>
-  )
+  return <>
+    <section className='h-screen'>
+      <h1 className='text-emerald-400 text-2xl font-bold md:mt-10'>ERROR 404</h1>
+      <h2 className=' text-xl font-semibold text-white mt-3'>Sorry This Page is not found</h2>
+    </section>
+  </>
 }
